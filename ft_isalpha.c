3419d77232012229f7797ae2e6d0d1c38145ebd8
Raya Gerdzhikova <rgerdzhi@car7s6.42barcelona.com>
2024-06-22 21:40:49 +0200

    ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,22 +6,22 @@
 /*   By: rgerdzhi <rgerdzhi@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 20:51:17 by rgerdzhi          #+#    #+#             */
-/*   Updated: 2024/06/22 21:34:46 by rgerdzhi         ###   ########.fr       */
+/*   Updated: 2024/06/22 21:40:23 by rgerdzhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ctype.h>
-#include <stdio.h>
+/*#include <stdio.h>*/
 
 int	ft_isalpha(int c)
 {
 	if (c > 64 && c < 90 || c > 96 && c < 123)
-		return(1024);
+		return (1024);
 	else
-		return(0);
+		return (0);
 }
 
-
+/*
 int	main(int argc, char *argv[])
 {
 	char c;
@@ -30,3 +30,4 @@ int	main(int argc, char *argv[])
 	printf("isalph: %d\n", isalpha(c));
 	return (0);
 }
+*/
