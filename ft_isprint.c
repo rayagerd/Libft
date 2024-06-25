8b182c71566db411e4d8b8d023dc31c5c7560694
Raya Gerdzhikova <rgerdzhi@car10s3.42barcelona.com>
2024-06-25 16:12:02 +0200

    isprint

---
+++ ft_isprint.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgerdzhi <rgerdzhi@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 15:59:44 by rgerdzhi          #+#    #+#             */
+/*   Updated: 2024/06/25 16:11:36 by rgerdzhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctype.h>
+/*#include <stdio.h>*/
+
+int	ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+		return (16384);
+	else
+		return (0);
+}
+/*
+int	main(int argc, char *argv[])
+{
+	char c;
+	c = argv[1][0];
+	printf("my function: %d\n", ft_isprint(c));
+	printf("isprint: %d\n", isprint(c));
+	return (0);
+}
+*/
