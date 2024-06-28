e1a25bdde5d1d6471bbc32bbec7ea69a65ac22a0
Raya Gerdzhikova <rgerdzhi@car6s6.42barcelona.com>
2024-06-28 21:57:11 +0200

    upper/lower

---
+++ ft_toupper.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgerdzhi <rgerdzhi@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 21:39:25 by rgerdzhi          #+#    #+#             */
+/*   Updated: 2024/06/28 21:54:55 by rgerdzhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctype.h>
+/*#include <stdio.h>*/
+
+int	ft_toupper(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		c -= 32;
+	}
+	return (c);
+}
+/*
+int	main(int argc, char *argv[])
+{
+	char	c;
+	
+	c = argv[1][0];
+	c = toupper(c);
+	printf("Toupper is: %c\n", toupper(c));
+	c = ft_toupper(c);
+	printf("My function: %c\n", ft_toupper(c));
+	return 0;
+}
+*/
