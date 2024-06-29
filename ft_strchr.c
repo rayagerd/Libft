88ee3c2ff5f4ef152590dabdcee1e48b7e3f46f5
Raya Gerdzhikova <rgerdzhi@car5s4.42barcelona.com>
2024-06-29 16:38:49 +0200

    strrchr

---
+++ ft_strchr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgerdzhi <rgerdzhi@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/29 15:26:16 by rgerdzhi          #+#    #+#             */
+/*   Updated: 2024/06/29 16:18:59 by rgerdzhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*#include <stdio.h>*/
+#include <string.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	ch;
+
+	ch = (char)c;
+	while (*s)
+	{
+		if (*s == ch)
+			return ((char *)s);
+		s++;
+	}
+	if (ch == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+/*
+int main () 
+{
+   const char str[] = "Hello, World!123498652#%$%";
+   const char ch = 'l';
+   char *ret;
+
+   ret = strchr(str, ch);
+
+   printf("String after |%c| is - |%s|\n", ch, ret);
+   
+   ret = ft_strchr(str, ch);
+
+   printf("String after |%c| is - |%s|\n", ch, ret);
+   return(0);
+}
+*/
