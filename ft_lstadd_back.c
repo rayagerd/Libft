f322268531e0bf76092feda18fd443aa5f7ec41e
Raya Gerdzhikova <rgerdzhi@car7s4.42barcelona.com>
2024-07-17 18:50:02 +0200

    back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgerdzhi <rgerdzhi@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 18:32:33 by rgerdzhi          #+#    #+#             */
+/*   Updated: 2024/07/17 18:48:39 by rgerdzhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*back;
+
+	if (!new)
+		return ;
+	if (!lst)
+	{
+		*lst = new;
+		return ;
+	}
+	back = ft_lstlast(lst);
+	back->next = new;
+}
